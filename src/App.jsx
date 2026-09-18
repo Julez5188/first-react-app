651d8fea89875c7d9a7bb9f25e2eec3259191c70
Julia Maximov <julia.maximov18@gmail.com>
2026-09-17 20:50:31 -0400

    practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
 import './App.css';
-import Todo from './components/Todo.jsx'
-import Title from './components/Title.jsx' 
-import Modal from './components/Modal.jsx'
+import Todo from './components/Todo'
+import Title from './components/Title.jsx' ;
+import Modal from './components/Modal.jsx';
+import React, {useState} from 'react';
 
 function App() {
+  const [showModal, setShowModal] = useState(true)
+  setShowModal(false)
   return (
     <>
        <Title />
+       <div>
+        <input type="text" onChange={(event) => {
+          console.log(event.target.value)
+        }} />
+        <button onClick={() => setShowModal(true)}>Add todo</button>
+       </div>
        <div className="todo__wrapper">
-          <Todo title="Finish Frontend Simplified"/> 
-          <Todo title="Finish Interview Section"/>
-          <Todo title="Land a 100k Job"/>
+          <Todo 
+            title="Finish Frontend Simplified"
+            paragraph="Code along with Frontend Simplified step by step."
+          /> 
+          <Todo 
+            title="Finish Interview Section"
+            paragraph="Finish every interview question in the next 6 weeks."
+          />
+          <Todo 
+            title="Land a 100k Job"
+            paragraph="Apply to 100 jobs."
+          />
         </div>
-       <Modal para="Are you sure?"/> 
+       {showModal && <Modal title="Confirm Delete?" />}
     </>
   );
 }
